1ce507b327be3c0b80d157bc196a30d5a519ee36
Dmitrii Mikailov <dmi.sm.rf@yandex.ru>
2022-09-07 00:00:17 +0300

    Add Сonstant.py file

---
+++ brain_games/games/Сonstant.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Constants.py
+"""
+WINSCORE = 3
+QUESTION = 'Question: '
+CORRECT = 'Correct!'
+WRONG_ANSWER = ' is wrong answer ;(. Correct answer was '
+TRY_AGAIN = 'Let''s try again, '
+CONGRATULATIONS = 'Congratulations, '
+OPERANDS = '+-*'
