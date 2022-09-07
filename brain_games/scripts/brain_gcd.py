8ba604a8d29689f038754adf8527c869fcbcd696
Dmitrii Mikailov <dmi.sm.rf@yandex.ru>
2022-09-07 22:18:45 +0300

    Add brain_gcd.py script

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.games.calc_game import gcd
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    gcd()
+
+
+if __name__ == '__main__':
+    main()
