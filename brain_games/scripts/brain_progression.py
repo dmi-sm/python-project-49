0548948a1347310f15c9b9b5289f5e9546843b71
Dmitrii Mikailov <dmi.sm.rf@yandex.ru>
2022-09-08 00:20:35 +0300

    Add brain_progression.py script

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.games.progression_game import progression
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    progression()
+
+
+if __name__ == '__main__':
+    main()
