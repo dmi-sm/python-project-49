ecd50e96b61b519640a37951d32d5ce4f723e196
Dmitrii Mikailov <dmi.sm.rf@yandex.ru>
2022-09-10 23:28:48 +0300

    Mv progression_game.py to progression.py and fix

---
+++ brain_games/games/progression.py
@@ -0,0 +1,19 @@
+from random import randint
+
+GAME_RULE = 'What number is missing in the progression?'
+
+
+def progression(start_progression, step_progression, length_progression):
+    return [str(i) for i in range(start_progression, start_progression +
+            length_progression * step_progression, step_progression)]
+
+
+def game():
+    start_progression = randint(1, 10)
+    step_progression = randint(1, 10)
+    length_progression = 5
+    missing_number_index = randint(0, length_progression - 1)
+    task = progression(start_progression, step_progression, length_progression)
+    answer = task[missing_number_index]
+    task[missing_number_index] = '..'
+    return (task, answer)
